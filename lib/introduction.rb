ebfa9d637f8043c513253fd571c3bb0c1b717da4
Selamawitl lemma <oneiconforlife26@gmail.com>
2020-08-19 19:37:10 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,5 +1,5 @@
 
 
-def introduction_with_language(name, language )
+def introduction_with_language("name", "language" )
   puts"Hi, my name is #{name} Iam learning to progeraming in #{language}. "
 end
